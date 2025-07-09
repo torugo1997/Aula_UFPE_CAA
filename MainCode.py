c239159d0a1ff7a54b93871193f9dc1de356be89
torugo1997 <119549387+torugo1997@users.noreply.github.com>
2025-07-09 14:27:38 -0300

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -1,39 +1,15 @@
-"""
-Victor Hugo Resende Lima
-vhugoreslim@gmail.com
-
-Esse código necessita dos seguintes pacotes nas respectivas versões:
-Pillow==9.0.1
-scipy==1.7.3
-streamlit==1.5.0
-click==8
-"""
 import streamlit as st
 import numpy as np
-import sys
-from streamlit import cli as stcli
-from scipy.integrate import quad #Single integral
-from scipy.integrate import dblquad
-from PIL import Image
 
-def main():
-    col1, col2, col3= st.columns(3)
-    foto = Image.open('ufpe.png')
-    col1.image(foto, width=130)
-    foto = Image.open('caa.jpg')
-    col3.image(foto, use_column_width=True)
-    
-    st.markdown("<h2 style='text-align: center; color: #306754;'>Avaliação Didática: Espaços vetoriais-Definição e propriedades</h2>", unsafe_allow_html=True)
-    
-    st.markdown("""
-        <div style="background-color: #F3F3F3; padding: 10px; text-align: center;">
-          <p style="font-size: 20px; font-weight: bold;">Aplicação web para Teste de Dependência Linear e Mudança de Base</p>
-          <p style="font-size: 15px;">By: Victor H. R. Lima</p>
-        </div>
-        """, unsafe_allow_html=True)
+st.title("Espaços Vetoriais: Dependência Linear e Mudança de Base")
 
-    aba = st.sidebar.radio("Escolha o módulo", ["Teste de Dependência Linear", "Mudança de Base"])
-    if aba == "Teste de Dependência Linear":
+# Menu de navegação
+aba = st.sidebar.radio("Escolha o módulo", ["Teste de Dependência Linear", "Mudança de Base"])
+
+# =========================
+# Aba 1: Dependência Linear
+# =========================
+if aba == "Teste de Dependência Linear":
     st.header("Teste de Dependência Linear")
     st.write("Digite os vetores como listas separadas por vírgulas.")
 
@@ -102,11 +78,4 @@ elif aba == "Mudança de Base":
             st.write(matriz_mudanca)
         except np.linalg.LinAlgError:
             st.error("A matriz da nova base não é invertível. Bases inválidas.")
-    
 
-
-if st._is_running_with_streamlit:
-    main()
-else:
-    sys.argv = ["streamlit", "run", sys.argv[0]]
-    sys.exit(stcli.main())
